b2b95f215c7429429600001ec1dfbcfbff6ad8e5
Santiago Gaviria <sgaviria@uc.edu.co>
2026-03-14 13:43:12 -0500

    Create installation route for system setup
    Add installation route to run migrations and seed database

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Route::get('/instalar-sistema', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true, '--seed' => true]);
+    \Illuminate\Support\Facades\Artisan::call('storage:link');
+    return '¡Instalación completada exitosamente! Ya puedes ir a /admin';
+});
